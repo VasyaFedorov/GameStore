8d00a7a80b5544a560e5e5bd36ff214227caf0f4
VasyaFedorov <fedorow.v2013@yandex.ru>
2022-03-15 12:41:06 +0300

    add file

---
+++ GameStoreServerSide/main.cpp
@@ -0,0 +1,22 @@
+#include "http_server.h"
+#include <QtCore/QCoreApplication>
+#include <iostream>
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication a(argc, argv);
+
+    Connection::init();
+    
+
+    http_server *serv = new http_server(&a);
+
+    if (!serv->isListening()) {
+        std::cout << "Server is not listening" << std::endl;
+        return 1;
+    }
+    else
+        std::cout << "Server is listening" << std::endl;
+
+    return a.exec();
+}
